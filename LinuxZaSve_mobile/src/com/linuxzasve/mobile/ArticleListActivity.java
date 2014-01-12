54ab6c607ebd9bddca126e1217a77cc6741d1de6
Dejan Peretin <dperetin@gmail.com>
2014-01-12 12:23:24 +0100

    Added ArcticleListActivity
    This Activity is the landing activity, loads ArticleListFragment.
    Purpose of this activity is to display a list of articles.

---
+++ LinuxZaSve_mobile/src/com/linuxzasve/mobile/ArticleListActivity.java
@@ -0,0 +1,28 @@
+package com.linuxzasve.mobile;
+
+import android.os.Bundle;
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+
+/**
+ * Activity displays list of articles (posts).
+ * @author dejan
+ *
+ */
+public class ArticleListActivity extends SherlockFragmentActivity {
+	
+	/**
+	 * Setting content view and creating action bar. 
+	 * Fragment that is being loaded is defined the view.
+	 */
+	@Override
+	public void onCreate(final Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_article_list);
+		
+		// Creating action bar and setting subtitle
+		ActionBar ab = getSupportActionBar();
+		ab.setSubtitle(getResources().getString(R.string.action_bar_subtitle_article_list));
+	}
+}
