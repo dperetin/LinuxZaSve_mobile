48cbc3d4cc04b2f1350aceb7d9749c69c67651a6
Dejan Peretin <dperetin@gmail.com>
2014-01-12 17:40:33 +0100

    Created ArticleSearchActivity activity

---
+++ LinuxZaSve_mobile/src/com/linuxzasve/mobile/ArticleListFragmentType.java
@@ -0,0 +1,11 @@
+package com.linuxzasve.mobile;
+
+/**
+ * ArticleListActivity and ArticleSearchActivity use the same fragment (ArticleListFragment).
+ * ArticleListFragmentType is passed to a ArticleListFragment constructor to tell them apart.
+ * @author dejan
+ *
+ */
+public enum ArticleListFragmentType {
+	LIST, SEARCH
+}
