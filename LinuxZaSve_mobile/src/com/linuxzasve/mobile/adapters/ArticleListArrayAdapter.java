11fe9b3c8a43712203212e8a27e188991925d6b0
Dejan Peretin <dperetin@gmail.com>
2014-09-11 17:18:53 +0200

    New files added

---
+++ LinuxZaSve_mobile/src/com/linuxzasve/mobile/adapters/ArticleListArrayAdapter.java
@@ -0,0 +1,68 @@
+package com.linuxzasve.mobile.adapters;
+
+import java.util.List;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.koushikdutta.urlimageviewhelper.UrlImageViewHelper;
+import com.linuxzasve.mobile.R;
+import com.linuxzasve.mobile.rest.model.Post;
+import com.linuxzasve.mobile.timthumb.TimThumb;
+
+public class ArticleListArrayAdapter extends ArrayAdapter<Post> {
+
+    private final List<Post> articleList;
+    private final Context context;
+
+    static class ViewHolder {
+        TextView articleTitle;
+        TextView publishDate;
+        TextView author;
+        TextView commentCount;
+        ImageView thumbnail;
+    }
+
+    public ArticleListArrayAdapter(final Context context, final List<Post> articleList) {
+        super(context, R.layout.article_list_item, articleList);
+        this.articleList = articleList;
+        this.context = context;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, final ViewGroup parent) {
+        ViewHolder holder;
+
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.article_list_item, parent, false);
+            holder = new ViewHolder();
+
+            holder.articleTitle = (TextView) convertView.findViewById(R.id.article_title);
+            holder.publishDate = (TextView) convertView.findViewById(R.id.publish_date);
+            holder.author = (TextView) convertView.findViewById(R.id.author);
+            holder.commentCount = (TextView) convertView.findViewById(R.id.comment_count);
+            holder.thumbnail = (ImageView) convertView.findViewById(R.id.thumbnail);
+
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        holder.articleTitle.setText(articleList.get(position).getTitle());
+        holder.publishDate.setText(articleList.get(position).getDate("dd.MM.yyyy"));
+        holder.author.setText(articleList.get(position).getAuthor().getNickname());
+        holder.commentCount.setText(Integer.toString(articleList.get(position).getComment_count()));
+
+        if ((articleList.get(position).getThumbnail_images() != null) && (articleList.get(position).getThumbnail_images().getFull() != null) && (articleList.get(position).getThumbnail_images().getFull().getUrl() != null)) {
+            String thumbnailUrl = TimThumb.generateTimThumbUrl(articleList.get(position).getThumbnail_images().getFull().getUrl(), 256, 256, 1);
+            UrlImageViewHelper.setUrlDrawable(holder.thumbnail, thumbnailUrl);
+        }
+
+        return convertView;
+    }
+}
---
+++ LinuxZaSve_mobile/src/com/linuxzasve/mobile/adapters/CommentListArrayAdapter.java
@@ -0,0 +1,149 @@
+package com.linuxzasve.mobile.adapters;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.text.Html;
+import android.text.Html.ImageGetter;
+import android.text.method.LinkMovementMethod;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.linuxzasve.mobile.R;
+import com.linuxzasve.mobile.emote.EmoticonDrawables;
+import com.linuxzasve.mobile.rest.model.Comment;
+
+public class CommentListArrayAdapter extends ArrayAdapter<Comment> {
+    private final Context context;
+    private final List<Comment> values;
+
+    public CommentListArrayAdapter(final Context context, final List<Comment> list) {
+        super(context, R.layout.comment_list_item, list);
+        this.context = context;
+
+        values = new ArrayList<Comment>();
+        List<Comment> tmp = new ArrayList<Comment>();
+
+        // get all comments without parents
+        for (Comment c : list) {
+            if ("0".equals(c.getParent())) {
+                tmp.add(c);
+            }
+        }
+
+        // set all children
+        for (Comment c : list) {
+            if (!"0".equals(c.getParent())) {
+                insertChild(tmp, c);
+            }
+        }
+
+        // sort it all
+        sort(tmp);
+
+        // unroll
+        unroll(tmp, 0);
+
+    }
+
+    private void sort(final List<Comment> tmp) {
+        if (tmp == null) {
+            return;
+        }
+        Collections.sort(tmp, new Comparator<Comment>() {
+
+            @Override
+            public int compare(final Comment e1, final Comment e2) {
+                return e1.getDate().compareTo(e2.getDate());
+            }
+
+        });
+
+        for (Comment c : tmp) {
+            sort(c.getChildren());
+        }
+    }
+
+    private void unroll(final List<Comment> tmp, final int depth) {
+        if (tmp == null) {
+            return;
+        }
+        for (Comment c : tmp) {
+            c.setDepth(depth);
+            values.add(c);
+
+            unroll(c.getChildren(), depth + 1);
+        }
+    }
+
+    private void insertChild(final List<Comment> list, final Comment comment) {
+        if (list == null) {
+            return;
+        }
+
+        for (Comment c : list) {
+            if (comment.getParent().equals(String.valueOf(c.getId()))) {
+                c.addChild(comment);
+            } else {
+                insertChild(c.getChildren(), comment);
+            }
+        }
+    }
+
+    @Override
+    public View getView(final int position, final View convertView, final ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(R.layout.comment_list_item, parent, false);
+        TextView view = (TextView) rowView.findViewById(R.id.separator_line);
+        view.setWidth((position + 1) * 12);
+
+        LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) view.getLayoutParams();
+        params.setMargins((values.get(position).getDepth() + 1) * 12, 0, 0, 0);
+        view.setLayoutParams(params);
+
+        TextView neki_tekst = (TextView) rowView.findViewById(R.id.comment_text);
+        neki_tekst.setMovementMethod(LinkMovementMethod.getInstance());
+        TextView datum = (TextView) rowView.findViewById(R.id.comment_publish_date);
+        TextView autor = (TextView) rowView.findViewById(R.id.comment_author);
+
+        datum.setText(values.get(position).getDate());
+        autor.setText(values.get(position).getName());
+
+        neki_tekst.setText(Html.fromHtml(values.get(position).getContent(), new ImageGetter() {
+            @Override
+            public Drawable getDrawable(final String source) {
+
+				/*
+                 * Dohvacam ID slike. Ako nema takovg smajla, vracam null
+				 */
+                Integer id = EmoticonDrawables.getDrawableId(source);
+
+                if (id == null) {
+                    return null;
+                }
+
+                Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(), id);
+
+                Drawable d = new BitmapDrawable(context.getResources(), bitmap);
+                int dWidth = d.getIntrinsicWidth();
+                int dHeight = d.getIntrinsicHeight();
+
+                d.setBounds(0, -dHeight, dWidth, 0);
+                return d;
+            }
+        }, null));
+
+        return rowView;
+    }
+}
